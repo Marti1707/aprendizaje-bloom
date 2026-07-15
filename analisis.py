54d4d27404cee38dd8c4e8594b976ec5dd2c7800
Marti1707 <ceahernandezmartina@gmail.com>
2026-07-15 16:20:02 -0400

    agregar filtro de datos

---
+++ analisis.py
@@ -0,0 +1,19 @@
+##
+import csv
+
+with open('ocean-temps.csv', mode='r') as archivo:
+    lector_csv = csv.reader(archivo)
+    cabecera = next(lector_csv)
+    
+    for fila in lector_csv:
+        temperatura_texto = fila[1]
+        
+        # La temperatura es distinta de MM
+        if temperatura_texto != "MM":
+            # Si no es MM con (float) transformamos a número decimal
+            temperatura = float(temperatura_texto)
+            
+            # temperatura ya convertida
+            print("Temperatura válida:", temperatura)
+
+        
