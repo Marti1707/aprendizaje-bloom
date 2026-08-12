bbdabc80ba151bc875ce8166e47516d05bc57ba5
Marti1707 <ceahernandezmartina@gmail.com>
2026-08-12 13:16:33 -0400

    Filtrado de temperaturas

--- analisis-sem3.py
+++ analisis-sem3.py
@@ -1,11 +1,12 @@
-import csv
+# Mision 1
 import csv
 
+# define la funcion leer_datos y recibe una variable que es la ruta del archivo que va a procesar
 def leer_datos(nombre_archivo):
-    datos = []
+    datos = [] # crea una lista vacia (sera para guardar la informacion filtrada de cada fila)
     with open(nombre_archivo, mode='r') as archivo:
         lector = csv.reader(archivo)
-        next(lector)  # Salta la cabecera (date, site, sea_surface_temp_c)
+        next(lector)  # Salta la cabecera
         
         for fila in lector:
             if len(fila) == 3:
@@ -13,7 +14,7 @@ def leer_datos(nombre_archivo):
                 sitio = fila[1]
                 temp_texto = fila[2]
                 
-                # Descartamos las filas que tienen "MM"
+                # se descartan las filas que tienen "MM"
                 if temp_texto != "MM":
                     datos.append({
                         'date': fecha,
@@ -38,3 +39,24 @@ promedio_total = promedio(lista_temperaturas)
 
 print("Total de registros válidos:", len(datos_cargados))
 print("Promedio general de temperatura:", promedio_total)
+
+# Mision 2: Función para filtrar temperaturas
+def temps_de_sitio(datos, sitio):
+
+    temperaturas_filtradas = []
+    for registro in datos:
+        if registro['site'] == sitio:
+            temperaturas_filtradas.append(registro['temperature'])
+    return temperaturas_filtradas
+
+datos_cargados = leer_datos('ocean-temps-2sites.csv')
+
+temps_monterey = temps_de_sitio(datos_cargados, 'monterey')
+promedio_monterey = promedio(temps_monterey)
+
+temps_san_pedro = temps_de_sitio(datos_cargados, 'san_pedro')
+promedio_san_pedro = promedio(temps_san_pedro)
+
+print("--- RESULTADOS POR SITIO ---")
+print("Monterey -> Lecturas:", len(temps_monterey), "| Promedio:", promedio_monterey)
+print("San Pedro -> Lecturas:", len(temps_san_pedro), "| Promedio:", promedio_san_pedro)
